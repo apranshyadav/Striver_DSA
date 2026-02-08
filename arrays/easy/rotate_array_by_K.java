b933d2e19be2ac1b3c30fe0cb54e0a03d0e1345a
apranshyadav <apranshy@gmail.com>
2026-02-08 22:27:19 +0530

    arrays_basics

--- arrays/easy/rotate_array_by_K.java
+++ arrays/easy/rotate_array_by_K.java
@@ -7,17 +7,28 @@ public class rotate_array_by_K {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        int k = sc.nextInt();
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
         sc.close();
-        rotate_array_by_K_elements(arr);
+        rotate_array_by_K_elements(arr, k);
         for(int i:arr){
-            System.out.print(arr[i]+" ");
+            System.out.print(i+" ");
         }
     }
 
-    public static void rotate_array_by_K_elements(int[] arr){
-        
+    public static void rotate_array_by_K_elements(int[] arr, int k){
+        int n=arr.length;
+        int[] temp = new int[k];
+        for(int i=0; i<k; i++){
+            temp[i] = arr[i];
+        }
+        for(int i=k; i<n; i++){
+            arr[i-k] = arr[i];
+        }
+        for(int i=n-k; i<n; i++){
+            arr[i] = temp[i-(n-k)];
+        }
     }
 }
