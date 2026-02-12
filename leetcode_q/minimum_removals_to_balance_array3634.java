a5623ab71948d8282524c04d0f336973bc876b36
apranshyadav <apranshy@gmail.com>
2026-02-12 23:19:38 +0530

    arrays_basics

--- leetcode_q/minimum_removals_to_balance_array3634.java
+++ leetcode_q/minimum_removals_to_balance_array3634.java
@@ -12,11 +12,10 @@ class Solution {
             while ((long) nums[j] > (long) nums[i] * k) {
                 i++;
             }
-
             maxLen = Math.max(maxLen, j - i + 1); 
         }
 
-        return nums.length - maxLen; 
+        return nums.length - maxLen;  
     }
 }
 
